e256d316db3c6cb5bb040e58c2f9355351123106
ilsha <ilshat666.93@gmail.com>
2024-11-13 20:24:44 +0500

    Определена функция для отправки запроса и преобразования ответа в нужный формат

--- cataas.py
+++ cataas.py
@@ -1,15 +1,20 @@
-from tkinter import
+from tkinter import *
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
 
 def load_image(url):
-    response = requests.get()
+    response = requests.get(url)
     print(response)
+    image_data = BytesIO(response.content)
+    img = Image.open(image_data)
+    win_size = '400x500'
+    window.geometry(win_size)
+    return ImageTk.PhotoImage(img)
 
 window = Tk()
-window.geometry('500x500')
+#window.geometry('500x500')
 
 label = Label()
 label.pack()
@@ -17,8 +22,13 @@ label.pack()
 url = 'https://cataas.com/cat'
 img = load_image(url)
 
+
+
 if img:
     label.config(image=img)
-    label.image = img
+#    label.image = img
+
+but = Button(text='Загрузка картинки', load_image)
+but.pack()
 
 window.mainloop()

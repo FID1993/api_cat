0ebf951cab2a0c4a93a718d856e4be3a12aa46e4
ilsha <ilshat666.93@gmail.com>
2024-11-13 20:45:23 +0500

    Добавлено меню и удалена кнопка загрузки

--- cataas.py
+++ cataas.py
@@ -4,31 +4,32 @@ from PIL import Image, ImageTk
 from io import BytesIO
 
 
-def load_image(url):
+def load_image():
     response = requests.get(url)
     print(response)
     image_data = BytesIO(response.content)
     img = Image.open(image_data)
-    win_size = '400x500'
-    window.geometry(win_size)
-    return ImageTk.PhotoImage(img)
+    img.thumbnail((500, 500))
+    img = ImageTk.PhotoImage(img)
+    label.config(image=img)
+    label.image=img
 
 window = Tk()
-#window.geometry('500x500')
+window.geometry('500x530')
 
 label = Label()
 label.pack()
 
 url = 'https://cataas.com/cat'
-img = load_image(url)
+
+mainmenu = Menu(window)
+window.config(menu=mainmenu)
+file_menu=Menu(mainmenu, tearoff=0)
+mainmenu.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузка изображения', command=load_image)
+file_menu.add_command(label='Выход', command=window.destroy)
 
 
-
-if img:
-    label.config(image=img)
-#    label.image = img
-
-but = Button(text='Загрузка картинки', load_image)
-but.pack()
+load_image()
 
 window.mainloop()
